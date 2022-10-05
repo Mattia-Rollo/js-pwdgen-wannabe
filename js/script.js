abac77d806017d60a34bdc142283979d19ac67d3
mattia.rollo <ste.tia@hotmail.it>
2022-10-05 15:26:51 +0200

    add index.html

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+const nameUser = prompt('come ti chiami?');
+console.log(nameUser);
+const surnameUser = prompt('qual\'è il tuo cognome?');
+console.log(surnameUser);
+const age = prompt('quanti ani hai?');
+console.log(age);
+const favoriteColor = prompt('qual\'è il tuo colore preferito?');
+console.log(favoriteColor);
+
+const generatedPassword = nameUser + surnameUser + age + favoriteColor;
+console.log(generatedPassword);
+
+const elemento = document.getElementsByClassName('new-password');
+console.log(elemento);
+elemento.innerHTML = `
+    <span> la password generata è :</span>
+    ${generatedPassword}
+
+`;
+
+
